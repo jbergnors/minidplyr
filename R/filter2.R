6807cc68dd1462e5e60ba51f35731aa60a71493f
Jesper Berg Nors <au458967@uni.au.dk>
2022-09-22 11:11:01 +0200

    init

---
+++ R/filter2.R
@@ -0,0 +1,12 @@
+#' Title
+#'
+#' @param df 
+#' @param ... 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+filter2 <- function(df, ...){
+      df[..., ]
+}
---
+++ tests/testthat/test-select2.R
@@ -0,0 +1,5 @@
+test_that("select2() works", {
+  df <- iris[1:3]
+  testthat::expect_equal(, )
+  testthat::expect_equal(2 * 2, 4)
+})
